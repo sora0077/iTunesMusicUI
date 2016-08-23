5f8569489d602024bb9399d04e11a7526e6c2435
林達也 <t.hayashi0077+github@gmail.com>
2016-08-23 23:19:22 +0900

    Initial Commit

---
+++ iTunesMusicUI/iTunesMusicUI.h
@@ -0,0 +1,19 @@
+//
+//  iTunesMusicUI.h
+//  iTunesMusicUI
+//
+//  Created by 林達也 on 2016/08/23.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iTunesMusicUI.
+FOUNDATION_EXPORT double iTunesMusicUIVersionNumber;
+
+//! Project version string for iTunesMusicUI.
+FOUNDATION_EXPORT const unsigned char iTunesMusicUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iTunesMusicUI/PublicHeader.h>
+
+
